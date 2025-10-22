d6cb9cae267c0c42072b5c96fa7fae659d6bf82b
Milena <mile.26souza08@gmail.com>
2025-10-22 09:48:54 -0300

    feito o texto equipe e logo

--- Frontend/src/pages/Equipe.jsx
+++ Frontend/src/pages/Equipe.jsx
@@ -1,25 +1,46 @@
 import React from "react";
-import ImagemEquipe from '../../assets/imgEquipe.png';
-import LogoG3 from '../../assets/logoG3.png'; 
+import ImagemEquipe from '../assets/imgEquipe.png';
+import LogoG3 from '../assets/logoG3.png'; 
+import Laura from '../assets/ImgR1.png';
+import Lucas from  '../assets/ImgR2.png';
+import Milena from '../assets/ImgR3.png';
+import Pietro from '../assets/ImgR4.png';
+import Pyetro from '../assets/ImgR5.png';
+import Vitor from '../assets/ImgR6.png';
 
+import './Equipe.css'
 export default function Equipe() {
+     const integrantes = [
+    { nome: "Laura Betti", img: Laura },
+    { nome: "Lucas Casagrande", img: Lucas },
+    { nome: "Milena", img: Milena },
+    { nome: "Pietro Melle", img: Pietro },
+    { nome: "Pyetro Joaquim", img: Pyetro },
+    { nome: "Vitor Geraldo", img: Vitor },
+  ];
+
   return (
     <article>
       <section>
         <section>
-         <img src={ImagemEquipe} alt="Logo do grupo abacate" />
-          <p>
+             <section className="equipe-img-texto">
+         <img src={ImagemEquipe} alt="Logo do grupo abacate" className="ImgEquipe"/>
+         <section className="blequipe-texto">
+            <h2 className="Titulo-Equipe">Quem Somos Nós?</h2>
+          <p className="Texto-Equipe">
             O grupo 3 é formado por Laura Betti, Lucas Casagrande, Milena,
             Pietro Melle, Pyetro Joaquim e Vitor Geraldo. Nosso grupo se destaca
             por valorizar três princípios fundamentais: honestidade,
             comprometimento e união. Acreditamos que a honestidade é essencial
-            para manter a confiança e o respeito entre os integrantes; o
+            para manter a confiança e o respeito entre os integrantes  o
             comprometimento demonstra nossa dedicação em cada atividade e desejo
-            de alcançar sempre o melhor resultado; já a união é o que fortalece
+            de alcançar sempre o melhor resultado já a união é o que fortalece
             nossa equipe, tornando o trabalho mais leve, produtivo e harmonioso.
             Esses valores guiam o grupo 3 mesmo diante de desafios, e é por isso
             que cada conquista se torna ainda mais significativa.
           </p>
+          </section>
+          </section>
         </section>
       </section>
 
@@ -28,23 +49,34 @@ export default function Equipe() {
         <h2 >
           Integrantes da Equipe e Suas Participações
         </h2>
+
+        <div className="equipe-container">
+      {integrantes.map((i) => (
+        <div className="integrante" key={i.nome}>
+          <img src={i.img} alt={i.nome} />
+          <p>{i.nome}</p>
+        </div>
+      ))}
+    </div>
         </section>
 
       
-      <section >
-        <h3>Explicação da Logo do Grupo</h3>
-        <p >
+      <section className="equipe-texto-logo" >
+        <section className="blequipe-textologo">
+        <h3 className="Titulo-Logo">Explicação da Logo do Grupo</h3>
+        <p className="texto-expli" >
           O abacate da logo do nosso grupo foi criado a partir de uma
           brincadeira de mímica feita em aula. No centro dele estão os nomes dos
           integrantes, e por fora há palavras que representam as características
           do grupo: honestidade, comprometimento e união. O abacate simboliza
           algo simples, natural e cheio de energia — assim como nossa equipe.
         </p>
+        </section>
+        <section>
+       <img src={LogoG3} alt="Logo do grupo abacate" className="Img-Logo"/>
+      </section>
       </section>
 
-      <section>
-       <img src={LogoG3} alt="Logo do grupo abacate" />
-      </section>
     </article>
   );
 }
