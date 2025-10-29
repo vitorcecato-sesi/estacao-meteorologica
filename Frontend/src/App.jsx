97c3c0228233f8c0d212ea435e298c9a6b16c320
PietroMelle <mellepietro3@gmail.com>
2025-10-28 21:58:40 -0300

    pagina hardware resposivada

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,15 +6,28 @@ import "./App.css";
 
 // Páginas
 import Home from "./pages/Home";
+// import Projeto from "./pages/Projeto";
+// import Simulacao from "./pages/Simulacao";
 import Hardware from "./pages/Hardware";
-import PerguntasRespostas from './pages/PerguntasRespostas'
+// import Software from "./pages/Software";
+// import PerguntasRespostas from "./pages/PerguntasRespostas";
+// import Resultados from "./pages/Resultados";
+// import Equipe from "./pages/Equipe";
 
+// Componente App
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} /> 
+          {/* <Route path='/projeto' element={<Projeto/>} /> */}
+          {/* <Route path='/simulacao' element={<Simulacao/>} /> */}
+          <Route path='/hardware' element={<Hardware/>} />
+          {/* <Route path='/software' element={<Software/>} />
+          <Route path='/perguntasRespostas' element={<PerguntasRespostas/>} />
+          <Route path='/resultados' element={<Resultados/>} />
+          <Route path='/equipe' element={<Equipe/>} /> */}
         </Routes>
       </BrowserRouter>
     </>
