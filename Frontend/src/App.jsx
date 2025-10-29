52fb41cc0a7bff50c531117a87dbcd74e12a99d8
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-10-29 09:36:36 -0300

    Consertando as rotas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,29 +6,28 @@ import "./App.css";
 
 // Páginas
 import Home from "./pages/Home";
-// import Projeto from "./pages/Projeto";
-// import Simulacao from "./pages/Simulacao";
+import Projeto from "./pages/Projeto";
+import Simulacao from "./pages/Wokwi";
 import Hardware from "./pages/Hardware";
-// import Software from "./pages/Software";
-// import PerguntasRespostas from "./pages/PerguntasRespostas";
-// import Resultados from "./pages/Resultados";
-// import Equipe from "./pages/Equipe";
+import Software from "./pages/Software";
+import PerguntasRespostas from "./pages/PerguntasRespostas";
+import Resultados from "./pages/Resultados";
+import Equipe from "./pages/Equipe";
 
 // Componente App
 function App() {
   return (
     <>
       <BrowserRouter>
-      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} /> 
-          {/* <Route path='/projeto' element={<Projeto/>} /> */}
-          {/* <Route path='/simulacao' element={<Simulacao/>} /> */}
+          <Route path='/projeto' element={<Projeto/>} />
+          <Route path='/simulacao' element={<Simulacao/>} />
           <Route path='/hardware' element={<Hardware/>} />
-          {/* <Route path='/software' element={<Software/>} />
+          <Route path='/software' element={<Software/>} />
           <Route path='/perguntasRespostas' element={<PerguntasRespostas/>} />
           <Route path='/resultados' element={<Resultados/>} />
-          <Route path='/equipe' element={<Equipe/>} /> */}
+          <Route path='/equipe' element={<Equipe/>} />
         </Routes>
       </BrowserRouter>
     </>
